4a19fbf88766e49e647ebc2e70fae0c1257c6534
c20321456 <C20321456@mytudublin.ie>
2022-10-18 13:16:26 +0000

    finished lab

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Press to add note'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
